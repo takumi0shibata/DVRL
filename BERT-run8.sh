2f544ca29736b465fcf290edcea5968c4e714de2
Takumi Shibata <shibata@ai.lab.uec.ac.jp>
2024-04-07 01:38:32 +0900

    update paes and bert

--- BERT-run8.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-python BERT-DVRL.py --test_prompt_id 8 --max_length 512 --batch_size 16 --epochs 10 --model_name bert-base-uncased
-for seed in 12 22 32 42 52
-do  
-    python BERT-DevOnly.py --test_prompt_id 8 --seed ${seed} --max_length 512 --batch_size 16 --epochs 30 --model_name bert-base-uncased
-    python BERT-FullSource.py --test_prompt_id 8 --seed ${seed} --max_length 512 --batch_size 16 --epochs 10 --model_name bert-base-uncased
-done
